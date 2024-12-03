9edaafc19e62f7aa0a3669e81bf08d4607c7c1b5
a.danilov <a.danilov@upstars.com>
2024-12-03 13:45:49 +0200

    UN-153

--- src/store/freshChat.ts
+++ src/store/freshChat.ts
@@ -1,5 +1,4 @@
 import { useUserInfo } from "@store/user/userInfo";
-import config from "@theme/configs/config";
 import { defineStore, storeToRefs } from "pinia";
 import { computed, ref } from "vue";
 
@@ -19,60 +18,62 @@ interface IFreshChatData {
     token: string;
 }
 
-export const useFreshchatStore = defineStore("freshchatStore", () => {
+export function createFreshChatStore(config: Record<string, unknown>) {
+    return defineStore("freshchatStore", () => {
 
-    const newMessagesCount = ref(0);
+        const newMessagesCount = ref(0);
 
-    const getMessagesCount = computed(() => {
-        return newMessagesCount.value;
-    });
+        const getMessagesCount = computed(() => {
+            return newMessagesCount.value;
+        });
 
-    function setNewMessageCount(count: number) {
-        newMessagesCount.value = count;
-    }
-
-    const { getUserInfo: userInfo } = storeToRefs(useUserInfo());
-
-    const userData = computed<ISanitizedUserData | null>(() => {
-        const { id, mobile_phone, email, first_name, last_name } = userInfo.value;
-        if (id) {
-            return {
-                externalId: `${ id }`,
-                email,
-                firstName: first_name,
-                lastName: last_name,
-                phone: mobile_phone,
-            };
+        function setNewMessageCount(count: number) {
+            newMessagesCount.value = count;
         }
-        return null;
-    });
 
-    const freshChatData = computed<IFreshChatData>(() => {
-        const {
-            getFreshChatRestoreId: restoreId,
-            getFreshChatRestoreIdLoaded: restoreIdLoaded,
-            getDataIsLoaded: isLoaded
-        } = storeToRefs(useUserInfo());
+        const { getUserInfo: userInfo } = storeToRefs(useUserInfo());
 
-        const externalId = userInfo.value.id;
+        const userData = computed<ISanitizedUserData | null>(() => {
+            const { id, mobile_phone, email, first_name, last_name } = userInfo.value;
+            if (id) {
+                return {
+                    externalId: `${ id }`,
+                    email,
+                    firstName: first_name,
+                    lastName: last_name,
+                    phone: mobile_phone,
+                };
+            }
+            return null;
+        });
+
+        const freshChatData = computed<IFreshChatData>(() => {
+            const {
+                getFreshChatRestoreId: restoreId,
+                getFreshChatRestoreIdLoaded: restoreIdLoaded,
+                getDataIsLoaded: isLoaded
+            } = storeToRefs(useUserInfo());
+
+            const externalId = userInfo.value.id;
+
+            return {
+                token: config.freshChat.token,
+                widgetUuid: config.freshChat.widgetUuid,
+                // restore have no impact without externalId
+                restoreId: externalId ? restoreId.value : undefined,
+                // externalId cause generating new restoreId, so we need wait initializing
+                // our saved restoreId to prevent overriding
+                externalId: restoreIdLoaded.value? externalId : undefined,
+                ...(userData.value || {}),
+                pending: isLoaded.value && !restoreIdLoaded.value,
+            };
+        });
 
         return {
-            token: config.freshChat.token,
-            widgetUuid: config.freshChat.widgetUuid,
-            // restore have no impact without externalId
-            restoreId: externalId ? restoreId.value : undefined,
-            // externalId cause generating new restoreId, so we need wait initializing
-            // our saved restoreId to prevent overriding
-            externalId: restoreIdLoaded.value? externalId : undefined,
-            ...(userData.value || {}),
-            pending: isLoaded.value && !restoreIdLoaded.value,
+            getMessagesCount,
+            setNewMessageCount,
+            userData,
+            freshChatData,
         };
     });
-
-    return {
-        getMessagesCount,
-        setNewMessageCount,
-        userData,
-        freshChatData,
-    };
-});
+}
