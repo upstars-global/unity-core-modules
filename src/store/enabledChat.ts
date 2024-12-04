6a373861fcf8f6a7c89919760c5a1306bd59850a
a.danilov <a.danilov@upstars.com>
2024-12-04 17:12:23 +0200

    UN-153 add config to chat store

--- src/store/enabledChat.ts
+++ src/store/enabledChat.ts
@@ -6,21 +6,24 @@ export const CHAT_LIVECHAT = "liveChat";
 export const DEFAULT_CHAT = "freshChat";
 export const RESERVE_CHAT = CHAT_LIVECHAT;
 
-export const useEnabledChatStore = defineStore("enabledChatStore", () => {
-    const enabledChat = computed(() => {
-        const levelsStore = useLevelsStore();
 
-        if (levelsStore.groups) {
-            const isEnableReserveChat = levelsStore.groups.find(({ id }) => {
-                return Number(id) === 545;
-            });
-            return isEnableReserveChat?.writable ? RESERVE_CHAT : DEFAULT_CHAT;
-        }
+export function createEnabledChatStore(chatId: number) {
+    return defineStore("enabledChatStore", () => {
+        const enabledChat = computed(() => {
+            const levelsStore = useLevelsStore();
 
-        return null;
+            if (levelsStore.groups) {
+                const isEnableReserveChat = levelsStore.groups.find(({ id }) => {
+                    return Number(id) === chatId;
+                });
+                return isEnableReserveChat?.writable ? RESERVE_CHAT : DEFAULT_CHAT;
+            }
+
+            return null;
+        });
+
+        return {
+            enabledChat,
+        };
     });
-
-    return {
-        enabledChat,
-    };
-});
+}
