47ae683344515823422e7817759240a50abd1ca9
d-tashchi-upstars <153002520+d-tashchi-upstars@users.noreply.github.com>
2024-12-19 11:53:53 +0300

    UN-98: - Fix scroll behavior by updating overflow handling (#43)
    Updated the `overflow` property to ensure correct scrolling behavior
    when toggling `bodyDisableScroll`. This change resolves potential
    inconsistencies in user experience for paged or auto scrolling.
    
    Co-authored-by: d-tashchi <kabak133@gmail.com>

--- src/helpers/bodyDisableScroll.ts
+++ src/helpers/bodyDisableScroll.ts
@@ -7,10 +7,12 @@ export default (visible: boolean) => {
 
     if (visible) {
         body.style.top = `-${window.scrollY}px`;
+        body.style.overflow = "auto -webkit-paged-y";
         body.classList.add("block-scroll");
     } else {
         const scrollY = body.style.top;
         body.style.top = "";
+        body.style.overflow = "";
         body.classList.remove("block-scroll");
         window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
     }
