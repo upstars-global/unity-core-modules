1c4d639c22f20f15f021a8e9756d3959ee4d82b1
Denis Zavadskiy <d.zavadskyi@upstars.com>
2024-12-30 11:54:49 +0200

    Reapply "UN-138-4 (#40)" (#50)
    This reverts commit 17ef2a7722b63b12b8f8815c623feafc05bdecef.

--- src/store/enabledChat.ts
+++ src/store/enabledChat.ts
@@ -1,29 +1,28 @@
-import { useLevelsStore } from "@store/levels/levelsStore";
+import { CHAT_ID } from "@theme/configs/constantsFreshChat";
 import { defineStore } from "pinia";
 import { computed } from "vue";
+
+import { useLevelsStore } from "./levels/levelsStore";
 
 export const CHAT_LIVECHAT = "liveChat";
 export const DEFAULT_CHAT = "freshChat";
 export const RESERVE_CHAT = CHAT_LIVECHAT;
 
+export const useEnabledChatStore = defineStore("enabledChatStore", () => {
+    const enabledChat = computed(() => {
+        const levelsStore = useLevelsStore();
 
-export function createEnabledChatStore(chatId: number) {
-    return defineStore("enabledChatStore", () => {
-        const enabledChat = computed(() => {
-            const levelsStore = useLevelsStore();
+        if (levelsStore.groups) {
+            const isEnableReserveChat = levelsStore.groups.find(({ id }) => {
+                return Number(id) === CHAT_ID;
+            });
+            return isEnableReserveChat?.writable ? RESERVE_CHAT : DEFAULT_CHAT;
+        }
 
-            if (levelsStore.groups) {
-                const isEnableReserveChat = levelsStore.groups.find(({ id }) => {
-                    return Number(id) === chatId;
-                });
-                return isEnableReserveChat?.writable ? RESERVE_CHAT : DEFAULT_CHAT;
-            }
-
-            return null;
-        });
-
-        return {
-            enabledChat,
-        };
+        return null;
     });
-}
+
+    return {
+        enabledChat,
+    };
+});
