743181cbd9c372d809240bc01423af365da8ae02
upstars-dmshevchenko <dm.shevchenko@upstars.com>
2025-01-30 14:42:31 +0100

    Reapply "UN-451 default currency with client's ip (#61)" (#77)

--- src/store/common.ts
+++ src/store/common.ts
@@ -3,13 +3,13 @@ import { ENABLE_CURRENCIES } from "@theme/configs/currencies";
 import { defineStore, type Pinia, storeToRefs } from "pinia";
 import { computed, ref } from "vue";
 
-import { currencyOfCountry } from "../helpers/currencyOfCountry";
+import { isValidCurrency } from "../helpers/currencyOfCountry";
 import { getUserAgentPlatform, type IPlatformState } from "../helpers/userAgentPlatform";
 import type { IPlayerFieldsInfo } from "../models/common";
 import type { IStagByReferName, ISurveyConfig } from "../models/configs";
 import { Currencies } from "../models/enums/currencies";
+import type { ICurrentIP } from "../services/api/DTO/current-ip";
 import type { ICountries, ICryptoExchangeRates, ICurrencies, IProjectInfo } from "../services/api/DTO/info";
-import { http } from "../services/api/http";
 import { loadStagByReferNameReq, loadSurveyConfigReq } from "../services/api/requests/configs";
 import {
     loadCountriesReq,
@@ -20,13 +20,14 @@ import {
 import { useMultilangStore } from "./multilang";
 
 export interface ICommonStoreDefaultOptions {
-    defaultCurrency: string;
-    enableCurrencies: string[];
+  defaultCurrency: string;
+  enableCurrencies: string[];
 }
 
 export const useCommon = defineStore("common", () => {
     const platform = ref<IPlatformState>();
     const defaultCurrency = ref(config.currencyDefault);
+    const currentIpInfo = ref<ICurrentIP>();
     const currencies = ref<ICurrencies[]>([]);
     const enableCurrencies = ref<string[]>(ENABLE_CURRENCIES);
     const playerFieldsInfo = ref<IPlayerFieldsInfo>();
@@ -108,10 +109,14 @@ export const useCommon = defineStore("common", () => {
     });
 
     const getDefaultCurrency = computed(() => {
-        const { getUserGeo: codeByGeoIp } = storeToRefs(useMultilangStore());
-        const defaultCurrencyOfCountry = currencyOfCountry(codeByGeoIp.value) as string;
-        return defaultCurrencyOfCountry || defaultCurrency.value;
+        const ipCurrency = currentIpInfo.value?.default_currency;
+
+        return isValidCurrency(ipCurrency) ? ipCurrency : defaultCurrency.value;
     });
+
+    function setCurrentIpInfo(currency: ICurrentIP) {
+        currentIpInfo.value = currency;
+    }
 
     function setDefaultOptions(defaultOptions: ICommonStoreDefaultOptions) {
         defaultCurrency.value = defaultOptions.defaultCurrency;
@@ -168,10 +173,12 @@ export const useCommon = defineStore("common", () => {
 
         surveyConfig,
         loadSurveyConfig,
+        currentIpInfo,
 
         loadCountries,
         getCountries,
 
+        setCurrentIpInfo,
         loadCurrencies,
         currencies,
         enableCurrencies,
