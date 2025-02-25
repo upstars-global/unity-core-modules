8c40d0ecd1bfbc71b8593c64e9ede7b04dfe2adf
d-tashchi-upstars <153002520+d-tashchi-upstars@users.noreply.github.com>
2025-02-25 18:29:17 +0300

    test: standardize string quotes in release.config.js (#106)
    Switched all double quotes to single quotes for consistency. Updated related configurations and strings to align with the chosen style guide.
    
    Co-authored-by: d-tashchi <kabak133@gmail.com>

--- release.config.js
+++ release.config.js
@@ -32,6 +32,7 @@ export default {
                 { type: "BREAKING CHANGE", release: "major" },
             ],
         } ],
+
         [ "@semantic-release/release-notes-generator", {
             preset: "conventionalcommits",
             parserOpts: {
