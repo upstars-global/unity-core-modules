2d39a11781da2e8722643e1789a79050e2807ebd
Denis Zavadskiy <d.zavadskyi@upstars.com>
2025-04-10 13:04:35 +0300

    feat(UN-763): cashbox amount change logic (#127)
    * feat: UN-763: add currency config for deposit buttons
    
    * update currency codes
    
    * fix: UN-1015
    
    * fix: max value
    
    * fix down calculation
    
    * fix min value
    
    * fix

--- src/models/cashbox.ts
+++ src/models/cashbox.ts
@@ -1,3 +1,21 @@
 import type { Currencies } from "./enums/currencies";
 
 export type ICoinspaidAddresses = Record<Currencies, string>;
+
+export type CurrencyCode =
+    "EUR" | "USD" | "CAD" | "AUD" | "NZD" | "BRL" | "NOK" |
+    "JPY" | "BTC" | "LTC" | "ETH" | "BCH" | "DOG" | "USDT";
+
+export type Step = {
+  min: number;
+  max: number | "Infinity";
+  step: number;
+};
+
+export type CurrencyConfig = {
+  defaultAmount: number;
+  rounding: number;
+  steps: Step[];
+};
+
+export type CurrencyData = Record<CurrencyCode, CurrencyConfig>;
