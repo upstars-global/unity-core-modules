83922a2035d6ffd8f1fa06ec32cbd040c5632630
DT <kabak133@gmail.com>
2025-04-28 18:41:30 +0300

    test: UN-833 Add tests for gamesFavorite store and improve code form… (#154)
    * tests: UN-833 Add tests for gamesFavorite store and improve code formatting
    
    Implemented unit tests for gamesFavorite store to ensure functionality of main actions such as loading, adding, and deleting favorite games. Refactored and standardized header and code formatting for consistency.
    
    * Refactor favorite games API calls and optimize logic
    
    Replaced direct HTTP calls with dedicated functions (fetchFavoriteGames, fetchAddFavoriteGamesCount, fetchDeleteGameFromFavorites) for better reusability. Simplified and optimized the favorite games store logic. Introduced type safety for game versions with `AcceptsGamesVariants` and improved maintainability.
    
    * Refactor favorite games store and unit tests
    
    Replaced direct HTTP mocks with specific API request mocks for better modularity and readability. Streamlined data setup in tests and updated store logic for clarity and consistency. Improved error handling and removed redundant legacy code.

--- src/store/games/gamesFavorite.ts
+++ src/store/games/gamesFavorite.ts
@@ -1,11 +1,15 @@
-import type { AxiosResponse } from "axios";
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 
 import { log } from "../../controllers/Logger";
 import { type IGameItem, processGame } from "../../helpers/gameHelpers";
-import type { GameFavoriteIds, IGame } from "../../models/game";
-import { http } from "../../services/api/http";
+import type { GameFavoriteIds } from "../../models/game";
+import { AcceptsGamesVariants } from "../../services/api/DTO/gamesDTO";
+import {
+    fetchAddFavoriteGamesCount,
+    fetchDeleteGameFromFavorites,
+    fetchFavoriteGames,
+} from "../../services/api/requests/games";
 
 export const useGamesFavorite = defineStore("gamesFavorite", () => {
     const favoritesId = ref<GameFavoriteIds>([]);
@@ -15,23 +19,16 @@ export const useGamesFavorite = defineStore("gamesFavorite", () => {
 
     async function loadFavoriteGames() {
         try {
-            const favoriteGameHeaderFullData = { "accept-client": "application/vnd.s.v2+json" };
-            const favoriteGameHeaderId = { "accept-client": "application/vnd.s.v1+json" };
-
             const [
                 gamesFullData,
                 gamesID,
-            ] = await Promise.all(
-                [
-                    favoriteGameHeaderFullData,
-                    favoriteGameHeaderId,
-                ].map((headers) => {
-                    return http({ headers }).get("/api/player/favorite_games");
-                }),
-            ) as [ AxiosResponse<IGame[]>, AxiosResponse<number[]> ];
+            ] = await Promise.all([
+                fetchFavoriteGames(AcceptsGamesVariants.fullData),
+                fetchFavoriteGames(AcceptsGamesVariants.onlyID),
+            ]);
 
-            favoritesId.value = gamesID.data;
-            gamesFavoriteFullData.value = gamesFullData.data
+            favoritesId.value = gamesID;
+            gamesFavoriteFullData.value = gamesFullData
                 .map((game) => processGame(game, game.identifier));
         } catch (err) {
             log.error("LOAD_FAVORITE_GAMES_ERROR", err);
@@ -40,27 +37,17 @@ export const useGamesFavorite = defineStore("gamesFavorite", () => {
     }
 
     async function addGameToFavorites(idGame: number): Promise<void> {
-        try {
-            await http({ auth: true }).put(`/api/player/favorite_games/${idGame}`);
-            await loadFavoriteGames();
-        } catch (err) {
-            log.error("ADD_GAME_TO_FAVORITES_ERROR", err);
-            throw err;
-        }
+        await fetchAddFavoriteGamesCount(idGame);
+        await loadFavoriteGames();
     }
 
     async function deleteGameFromFavorites(idGame: number) {
-        try {
-            await http().delete(`/api/player/favorite_games/${idGame}`);
+        await fetchDeleteGameFromFavorites(idGame);
 
-            favoritesId.value = favoritesId.value.filter((idFavorite) => idFavorite !== idGame);
-            gamesFavoriteFullData.value = gamesFavoriteFullData.value.filter((gameFavorite) => {
-                return !Object.values(gameFavorite.currencies).some(({ id }) => id === idGame);
-            });
-        } catch (err) {
-            log.error("DELETE_GAME_FROM_FAVORITES_ERROR", err);
-            throw err;
-        }
+        favoritesId.value = favoritesId.value.filter((idFavorite) => idFavorite !== idGame);
+        gamesFavoriteFullData.value = gamesFavoriteFullData.value.filter((gameFavorite) => {
+            return !Object.values(gameFavorite.currencies).some(({ id }) => id === idGame);
+        });
     }
 
     const getGamesFavoriteFullData = computed(() => [ ...gamesFavoriteFullData.value ].reverse());
@@ -73,6 +60,7 @@ export const useGamesFavorite = defineStore("gamesFavorite", () => {
     return {
         gamesFavoriteID,
         gamesFavoriteFullData,
+        favoritesId,
 
         getGamesFavoriteFullData,
 
