17146cab49313af0c5ca70acb381ee3eac68f5c7
o-shlemko <o.shlemko@upstars.com>
2025-05-28 13:51:21 +0300

    feat(UN-1142): modified new endpoint for pwa events (#162)
    * feat(UN-1142): added new endpoint for pwa events
    
    * feat(UN-1142) added check for logged user for pwa events
    
    * feat(UN-1142): added subscription for mode standalone MQL
    
    * feat(UN-1142): added isServer check for MQL subscription

--- src/services/common.ts
+++ src/services/common.ts
@@ -1,3 +1,4 @@
+import { isServer } from "../helpers/ssrHelpers";
 import { useCommon } from "../store/common";
 import { usePWA } from "../store/pwa";
 import { useUserInfo } from "../store/user/userInfo";
@@ -13,11 +14,31 @@ export async function loadCurrentIP() {
     }
 }
 
-export async function sendPWAEvent(event: PWAEvent) {
-    const pwaStore = usePWA();
-    const userStore = useUserInfo();
-    pwaStore.setIsPWA();
-    if (pwaStore.isPWA && userStore.getIsLogged) {
-        await sendPWAEventReq(event);
+export async function subscribeToStandaloneMQL() {
+    if (!isServer) {
+        const pwaStore = usePWA();
+        const userStore = useUserInfo();
+        let hasBeenSent = false;
+
+        pwaStore.setIsPWA(); // setting default state;
+
+        if (pwaStore.isPWA) {
+            await sendPWAEvent("open");
+            hasBeenSent = true;
+        }
+
+        const standaloneMediaQuery = window.matchMedia("(display-mode: standalone)");
+
+        standaloneMediaQuery.addEventListener("change", async (event) => {
+            pwaStore.setIsPWA(event.matches);
+            if (event.matches && !hasBeenSent && userStore.getIsLogged) {
+                await sendPWAEvent("open");
+                hasBeenSent = true;
+            }
+        });
     }
+}
+
+export async function sendPWAEvent(event: PWAEvent) {
+    await sendPWAEventReq(event);
 }
