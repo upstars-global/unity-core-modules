b01c3bd3c5cd4c7a40161e9886a31d01bd1f836a
Maxim Ochkolyas <m.ochkolyas@upstars.com>
2025-07-18 01:00:50 +0300

    done

--- src/directives/filterExcludeNumbers.ts
+++ src/directives/filterExcludeNumbers.ts
@@ -1,26 +1,16 @@
-const regex = /\d+/g;
-
-const keypressHandler = (el): void => {
-    const char = String.fromCharCode(el.keyCode);
-
-    if (regex.test(char)) {
-        el.preventDefault();
-    }
-};
-
 const updateValue = (el): void => {
-    const currentElement = el.value !== undefined ? el : el.querySelector("input, textarea");
-    currentElement.value = currentElement.value.replace(regex, "");
+    el.value = el.value.replace(/[0-9]/g, "");
 };
+
+
+let currentElement = {};
 
 export const filterExcludeNumbers = {
     beforeMount(el) {
-        el.addEventListener("keypress", keypressHandler);
+        currentElement = el.value !== undefined ? el : el.querySelector("input, textarea");
+        updateValue(currentElement);
     },
-    updated(el) {
-        updateValue(el);
-    },
-    beforeUnmount(el) {
-        el.removeEventListener("keypress", keypressHandler);
+    updated() {
+        updateValue(currentElement);
     },
 };
