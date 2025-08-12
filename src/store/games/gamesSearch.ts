95a721c4a27fea8e76465acd226640570bdc741a
m-ochkolyas-upstars <m.ochkolyas@upstars.com>
2025-08-12 19:08:20 +0300

    revert: "refactor: gamesSearch" (#207)
    This reverts commit a5969859cdc21bdd432826f83a7db6956c4a9bda.

---
+++ src/store/games/gamesSearch.ts
@@ -0,0 +1,27 @@
+import { defineStore, storeToRefs } from "pinia";
+
+import { log } from "../../controllers/Logger";
+import { processGameForNewAPI } from "../../helpers/gameHelpers";
+import { http } from "../../services/api/http";
+import { useRootStore } from "../root";
+
+export const useGamesSearch = defineStore("gamesSearch", () => {
+    async function loadFoundGames(searchString: string) {
+        const { isMobile } = storeToRefs(useRootStore());
+        try {
+            const { data } = await http().post("/api/games_filter", {
+                device: isMobile.value ? "mobile" : "desktop",
+                filter: {
+                    title: searchString,
+                },
+            });
+            return data.data.map(processGameForNewAPI);
+        } catch (err) {
+            log.error("LOAD_FOUND_GAMES_ERROR", err);
+        }
+    }
+
+    return {
+        loadFoundGames,
+    };
+});
