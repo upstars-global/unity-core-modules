145c038e77052e03297784edc570f28efb995900
Maxim Ochkolyas <m.ochkolyas@upstars.com>
2025-08-15 20:17:50 +0300

    add filter first 10 items

--- src/services/providers.ts
+++ src/services/providers.ts
@@ -2,12 +2,22 @@ import type { IDisabledGamesProvider } from "../models/game";
 import { loadDisabledProvidersConfigReq } from "../services/api/requests/configs";
 import { useGamesProviders } from "../store/games/gamesProviders";
 
+interface IProvidersList {
+    [key: string]: string | string[];
+}
+
 export async function loadDisabledGamesProviders(): Promise<void> {
     const { setDisabledGamesProviders } = useGamesProviders();
 
-    const data = await loadDisabledProvidersConfigReq();
+    const data: IProvidersList = await loadDisabledProvidersConfigReq();
 
     if (data) {
-        setDisabledGamesProviders(data as IDisabledGamesProvider);
+        const first10Props: IProvidersList = {};
+
+        Object.entries(data).slice(0, 10).forEach(([ key, value ]) => {
+            first10Props[key] = value;
+        });
+
+        setDisabledGamesProviders(first10Props as IDisabledGamesProvider);
     }
 }
