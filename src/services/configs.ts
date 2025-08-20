e06b6549168af66a71c5f9f7ebfc62db676b3c57
a.danilov <a.danilov@upstars.com>
2025-08-20 19:36:03 +0300

    UN-1405 vip-program

--- src/services/configs.ts
+++ src/services/configs.ts
@@ -60,7 +60,7 @@ export async function loadVipProgramConfig() {
     const data = await loadVipProgramConfigReq();
 
     if (data) {
-        levelsStore.setRewardsData(data);
+        levelsStore.setConfigData(data);
     }
 }
 
