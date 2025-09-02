944b41d5b4159f52f8d06fe0a8ac5b038d101b3d
zeroequalzero <d.zavadskyi@upstars.com>
2025-09-02 11:08:03 +0300

    Reapply "feat: refactor lootboxes store, unit tests for lootboxes, roc… (#214)
    This reverts commit 57d1ab386302c647c6eb4a2b0b1a3c23f44bd77a.

--- src/services/api/requests/lootbox.ts
+++ src/services/api/requests/lootbox.ts
@@ -6,63 +6,44 @@ import { ILootboxesFileConfig } from "../DTO/lootboxes";
 import { http } from "../http";
 
 export async function loadMockLootboxWheelConfigs():
-    Promise<ILootboxesFileConfig | Error | undefined> {
+    Promise<ILootboxesFileConfig | undefined> {
     try {
         const { data } = await http().get<ILootboxesFileConfig>("/api/fe/config/wheel-config");
 
         return data;
     } catch (err) {
         log.error("LOAD_MOCK_SECTIONS_ROCKET_WHEEL_ERROR", err);
-        throw err;
     }
-} export async function loadMockLootboxWheelSegmentsConfigs():
-    Promise<Record<string, ILootboxesFileConfig> | Error | undefined> {
+}
+
+export async function loadMockLootboxWheelSegmentsConfigs():
+    Promise<Record<string, ILootboxesFileConfig> | undefined> {
     try {
         const { data } = await http().get<Record<string, ILootboxesFileConfig>>("/api/fe/config/wheel-config-segments");
 
         return data;
     } catch (err) {
         log.error("LOAD_MOCK_SECTIONS_ROCKET_WHEEL_ERROR", err);
-        throw err;
     }
 }
 
-export async function getLootboxes() {
+export async function getLootboxesReq() {
     try {
         const { data } = await http().get<ILootbox[]>("/api/player/lootboxes");
 
         return data;
     } catch (err) {
         log.error("LOAD_LOOTBOXES_LIST_ERROR", err);
-        throw err;
     }
 }
 
-export async function activateLootbox(id: number) {
+export async function activateLootboxReq(id: number) {
     try {
         const { data } = await http().post(`/api/player/lootboxes/${id}/activation`);
 
         return data;
     } catch (err) {
         log.error("LOAD_PRIZE_OF_LOOTBOX_ERROR", err);
-    }
-}
-
-export async function loadPageContentFromCmsReq(slugPage: UserGroup): Promise<IPageItemCMS | void> { // TODO: rename
-    try {
-        const { data } = await http().get<IPageItemCMS>(`/api/cms/pages/${ slugPage }`);
-        return data;
-    } catch (err) {
-        log.error("LOAD_PAGE_CONTENT_FORM_CMS_REQ_ERROR", err);
-    }
-}
-
-export async function loadPageContentFromWheelCmsReq(slug: string): Promise<IPageItemCMS | void> {
-    try {
-        const { data } = await http().get<IPageItemCMS>(`/api/cms/pages/${slug}`);
-        return data;
-    } catch (err) {
-        log.error("LOAD_PAGE_CONTENT_FORM_CMS_REQ_ERROR", err);
     }
 }
 
