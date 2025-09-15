f76f0c701e839f3915daf9494f7c12eabdd6d05a
Maxim Ochkolyas <m.ochkolyas@upstars.com>
2025-09-15 19:36:40 +0300

    done

--- src/controllers/utmParams.ts
+++ src/controllers/utmParams.ts
@@ -9,33 +9,9 @@ export const UTM_PARAMS = [
     "sub_id",
 ];
 
-export function setUtmParamsToCookies(query: Record<string, string | undefined> = {}, expires: number) {
-    if (typeof document === "undefined") {
-        return;
-    }
-
-    const now = new Date();
-    now.setTime(now.getTime() + expires);
-
-    const isSetCookies = Object.keys(query).some((queryItem) => {
-        return UTM_PARAMS.find((utm) => {
-            return utm === queryItem;
-        });
-    });
-
-    if (isSetCookies) {
-        UTM_PARAMS.forEach((key) => {
-            const cookieValue = query[key] || "";
-            CookieController.set(key, cookieValue, {
-                "expires": now.toUTCString(),
-                "path=/": true,
-            });
-        });
-    }
-}
-
 export function getUtmParamsFromCookies() {
     const utmParams = {} as Record<string, string | undefined>;
+
     UTM_PARAMS.forEach((key) => {
         utmParams[key] = CookieController.get(key);
     });
