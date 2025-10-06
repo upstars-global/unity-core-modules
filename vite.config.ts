614a3b3d0b6a7bec23ac34ba00e6d1e5464cba95
zeroequalzero <d.zavadskyi@upstars.com>
2025-10-06 12:45:26 +0300

    Revert "test: context & winners stores unit tests (#232)"
    This reverts commit 3a59714e429fed4531d2d0c9886ad5326e43c273.

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,6 @@ export default defineConfig({
             provider: "v8",
             reporter: [ "text", "json-summary", "json" ],
             include: [ "src/**/*.ts" ],
-            exclude: [ "src/services/api/**", "src/models/**" ],
             reportOnFailure: true,
         },
         alias: [
