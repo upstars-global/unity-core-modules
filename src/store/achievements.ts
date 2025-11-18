c8c73d3fbfe5763bfb2deac5a5804ec5229a2d46
zeroequalzero <d.zavadskyi@upstars.com>
2025-11-18 18:02:21 +0200

    test: achievements store tests

--- src/store/achievements.ts
+++ src/store/achievements.ts
@@ -2,11 +2,12 @@ import { ACHIEV_ID, defaultDepCount, TOURNAMENT_IDS_FOR_ACHIEV } from "@config/a
 import featureFlags from "@theme/configs/featureFlags";
 import dayjs from "dayjs";
 import { defineStore } from "pinia";
+import { type IUserStatus } from "src/models/user";
+import { type IStatuses } from "src/services/api/DTO/statuses";
 import { computed } from "vue";
 
 import { betSunCompletedInTour, containAchievIdInUserStatuses } from "../helpers/achievementHelpers";
 import { STATUS_PROMO } from "../models/enums/tournaments";
-import type { IStatus } from "../models/levels";
 import { useCashboxStore } from "./cashboxStore";
 import { useLevelsStore } from "./levels/levelsStore";
 import { useTournamentsStore } from "./tournaments/tournamentsStore";
@@ -14,13 +15,14 @@ import { useUserStatuses } from "./user/userStatuses";
 
 const defaultDepDateStartCount = "2022-03-29T00:00:00Z";
 
-type IAchievement = IStatus | {
+type IAchievement = Omit<IStatuses, "status" | "id"> & {
     frontend_identifier: string | number;
     status: string;
-    money_budget_cents: string
+    money_budget_cents: string;
+    id: number;
 };
 
-function showAchievByUserStatus(userStatuses): number[] {
+function showAchievByUserStatus(userStatuses: IUserStatus[]): number[] {
     const showAchievEmail = userStatuses.some((status) => {
         return Number(status.id) === 606 || Number(status.id) === ACHIEV_ID.EMAIL_CONFIRM;
     });
@@ -66,13 +68,18 @@ export const useAchievements = defineStore("achievements", () => {
     const getTournamentForAchiev = computed<IAchievement[]>(() => {
         return tournamentsStore.getAllTournamentsOnlyUser.filter((tour) => {
             return TOURNAMENT_IDS_FOR_ACHIEV.includes(tour.frontend_identifier);
-        });
+        }) as unknown as IAchievement[];
     });
 
     const getAchievementsAll = computed<IAchievement[]>(() => {
         // display/hide some achiev for user by status
         const arrayAchievId = showAchievByUserStatus(userStatuses.getUserStatuses);
-        const achievByGroups = levelsStore.groups.map((itemGroup) => {
+        levelsStore.getLevelsData.forEach((level) => {
+            if (!arrayAchievId.includes(Number(level.id))) {
+                arrayAchievId.push(Number(level.id));
+            }
+        });
+        const achievByGroups = levelsStore.groups.value.map((itemGroup) => {
             return {
                 ...itemGroup,
                 id: Number(itemGroup.id),
@@ -85,11 +92,11 @@ export const useAchievements = defineStore("achievements", () => {
         return [
             ...achievByGroups,
             ...getTournamentForAchiev.value,
-        ];
+        ] as IAchievement[];
     });
 
     const getDepCountForAchiev = computed<number>(() => {
-        const countCompletedDeps = cashboxStore.historyDeposits.filter((itemDep) => {
+        const countCompletedDeps = cashboxStore.historyDeposits.value.filter((itemDep) => {
             return dayjs(itemDep.finished_at).isAfter(dayjs(defaultDepDateStartCount)) &&
                 itemDep.success;
         });
@@ -98,7 +105,11 @@ export const useAchievements = defineStore("achievements", () => {
 
     const getAchievementsActive = computed<IAchievement[]>(() => {
         return getAchievementsAll.value.filter((itemAchiev) => {
-            if (itemAchiev.frontend_identifier && itemAchiev.status === STATUS_PROMO.ARCHIVE) {
+            if (
+                "frontend_identifier" in itemAchiev &&
+                itemAchiev.frontend_identifier &&
+                itemAchiev.status === STATUS_PROMO.ARCHIVE
+            ) {
                 return false;
             }
 
