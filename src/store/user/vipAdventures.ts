de4644e9772cdf8aaf0fde26f0118cfdf0224e90
Artem Oleinychenko <a.oleinychenko@upstars.com>
2026-01-05 13:12:36 +0200

    feat: (UN-2089) adventure - compare without hours

--- src/store/user/vipAdventures.ts
+++ src/store/user/vipAdventures.ts
@@ -97,6 +97,7 @@ export const useVipAdventures = defineStore("vipAdventures", () => {
     const superConfig = computed(() => {
         const calendarLength = calendarConfig.value.length;
         const lastCalendarDay = calendarConfig.value[calendarLength - 1];
+
         return {
             index: calendarConfig.value.length,
             today: dayjs(lastCalendarDay.fullDate, formatDateVipAdv).isBefore(toDay.value, "day"),
