c6b468f8bf7249d73d67180d382e7d620b1800e0
Maxim Ochkolyas <m.ochkolyas@upstars.com>
2026-03-05 21:08:51 +0200

    done

--- src/helpers/referrerHelper.ts
+++ src/helpers/referrerHelper.ts
@@ -1,12 +1,23 @@
+/* eslint-disable @stylistic/js/max-len */
 import { REFERRER_COOKIE_NAME } from "@theme/configs/constsCookies";
 
 import { CookieController } from "../controllers/CookieController";
 import { isServer } from "../helpers/ssrHelpers";
 
-export const referrerHelper = (): string | undefined => {
+export const getDocumentReferrer = (): string | undefined => {
     if (isServer) {
         return;
     }
 
     return document.referrer || CookieController.get(REFERRER_COOKIE_NAME);
+};
+
+export const getAIReferrer = (queryParams: URLSearchParams): string | undefined => {
+    const regexUTM: RegExp = /^(chatgpt|openai|gemini|bard|perplexity|claude|copilot|mistral|llama|ai21|grok|deepseek|qwen)(\.com)?(_ai)?$/;
+    const regexRef: RegExp = /chat\.openai\.com|chatgpt\.com|perplexity\.ai|claude\.ai|gemini\.google\.com|copilot\.microsoft\.com|chat\.mistral\.ai|x\.ai/;
+
+    const utmSourceQuery = queryParams?.get("utm_source");
+    const referrer = getReferrer();
+
+    return utmSourceQuery?.match(regexUTM)?.[0] || referrer?.match(regexRef)?.[0];
 };
