5d3225fb9a290b265ac4d93dbec232af1f76c8c4
Denis Zavadskiy <d.zavadskyi@upstars.com>
2026-04-16 10:50:48 +0300

    feat(UN-2604): add custom stag for ref program (#339)
    * feat(UN-2604): add custom stag for ref program
    
    * feat: read referralStag from defaultProjectConfig
    
    * tests

--- types/configProjectTypes.ts
+++ types/configProjectTypes.ts
@@ -3,4 +3,5 @@ import { Currencies } from "src/models/enums/currencies";
 export type UnityConfig = {
     featureFlags: Record<string, boolean>;
     disabledCurrencies: Currencies[];
+    referralStag: string | undefined;
 };
