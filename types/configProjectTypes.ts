87c1d289474ff61fbc5ddfc4b5ff3426cd128de3
Artem Oleinychenko <a.oleinychenko@upstars.com>
2026-05-13 16:47:46 +0300

    UN-1031

--- types/configProjectTypes.ts
+++ types/configProjectTypes.ts
@@ -1,13 +1,7 @@
 import { type Currencies } from "../src/models/enums/currencies";
 
-export type VipAdventuresConfig = {
-    groups: Array<string | number>;
-
-};
-
 export type UnityConfig = {
     featureFlags: Record<string, boolean>;
     disabledCurrencies: Currencies[];
     referralStag: string | undefined;
-    vipAdventures: VipAdventuresConfig;
 };
