182b58b4f5eaea4a29fa88699268e6266d0d40cf
Denis Zavadskiy <d.zavadskyi@upstars.com>
2026-05-13 12:01:38 +0300

    chore(UN-2555): types for vip program (#342)
    * chore: add types for vip program
    
    * chore: types for vip program
    
    * fix: tests

--- src/models/levels.ts
+++ src/models/levels.ts
@@ -2,6 +2,7 @@ import { type Currencies } from "./enums/currencies";
 
 type LifetimeLevel = `lifetime_level_${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8}`;
 type VipLevel = `vip_level_${1 | 2 | 3 | 4 | 5 | 6}`;
+export type VipLevelSave = `vip_level_${ 2 | 3 | 4 | 5 | 6}_save`;
 
 export type Level = LifetimeLevel | VipLevel;
 
@@ -30,7 +31,7 @@ export type Reward = RewardConfig & {
     id: string;
 }
 
-export type Rewards = Record<Level, Reward[]>;
+export type Rewards = Record<Level | VipLevelSave, Reward[]>;
 
 export type LevelConfig = {
     image: {
