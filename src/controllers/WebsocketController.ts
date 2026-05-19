61c98ae1cbd04f6a01c44c29c917b57fae33bcd6
zeroequalzero <d.zavadskyi@upstars.com>
2026-05-19 14:38:39 +0300

    feat(UN-2729): notification center flow

--- src/controllers/WebsocketController.ts
+++ src/controllers/WebsocketController.ts
@@ -1,16 +1,33 @@
 import { getCentrifugeUrl } from "@config/centrifuge";
-import Centrifuge from "centrifuge/centrifuge";
+import { Centrifuge } from "centrifuge";
+import CentrifugeLegacy from "centrifuge-legacy/centrifuge";
 import { storeToRefs } from "pinia";
 
+import log from "../controllers/Logger";
+import { type INotificationCenterSubscription } from "../services/api/DTO/notificationCenter";
+import { loadNotificationCenterSubscriptionReq } from "../services/api/requests/notificationCenter";
+import { loadProjectInfo } from "../services/common";
+import { useCommon } from "../store/common";
 import { useEnvironments } from "../store/environments";
 import { useUserInfo } from "../store/user/userInfo";
 
-let sock = {},
-    $bus = {};
+let sock: LegacyCentrifugeClient | null = null;
+let notificationCenterClient: Centrifuge | null = null;
+let $bus: Partial<IBus> = {};
 
+interface LegacyCentrifugeClient {
+    _clientID?: string;
+    _subs: Record<string, unknown>;
+    onmessage?: (response: { data: string }) => void;
+    connect(): void;
+    disconnect(): void;
+    subscribe(channel: string, callback: (message: unknown) => void): void;
+}
 
 export const CHANNELS_TYPE_PUBLIC = "public";
 export const CHANNELS_TYPE_PRIVATE = "private";
+
+type ChannelType = typeof CHANNELS_TYPE_PUBLIC | typeof CHANNELS_TYPE_PRIVATE;
 
 const CHANNELS_BY_TYPE = {
     [CHANNELS_TYPE_PUBLIC]: [
@@ -35,9 +52,9 @@ const CHANNELS_BY_TYPE = {
         "payment_methods_updated",
         "balance",
     ],
-};
+} as const;
 
-function init(bus) {
+function init(bus: IBus) {
     if (!bus) {
         return;
     }
@@ -45,59 +62,169 @@ function init(bus) {
     $bus = bus;
 }
 
-function subscribe(channelsType) {
+function subscribeLegacyChannels(channelsType: ChannelType) {
+    const client = sock;
+
+    if (!client) {
+        return;
+    }
+
     const { getSettings } = storeToRefs(useUserInfo());
-    const chanelSetting = channelsType === CHANNELS_TYPE_PRIVATE ? `#${ getSettings.value.cent.user }` : "";
+    const chanelSetting = channelsType === CHANNELS_TYPE_PRIVATE ? `#${getSettings.value?.cent.user}` : "";
+
     CHANNELS_BY_TYPE[channelsType].forEach((chanel) => {
-        const keySubs = `${ chanel }${ chanelSetting }`;
-        if (Object.keys(sock._subs).includes(keySubs)) {
+        const keySubs = `${chanel}${chanelSetting}`;
+
+        if (Object.keys(client._subs).includes(keySubs)) {
             return;
         }
-        sock.subscribe(keySubs, (message) => {
-            $bus.$emit(`websocket.${ chanel }`, message);
+
+        client.subscribe(keySubs, (message) => {
+            $bus.$emit?.(`websocket.${chanel}`, message);
         });
     });
+}
+
+async function startLegacyFlow() {
+    const { getSettings } = storeToRefs(useUserInfo());
+    const settings = getSettings.value;
+
+    if (!settings?.cent) {
+        return;
+    }
+
+    sock = new CentrifugeLegacy({
+        ...settings.cent,
+        url: getCentrifugeUrl(settings.cent.url),
+    }) as LegacyCentrifugeClient;
+
+    subscribeLegacyChannels(CHANNELS_TYPE_PUBLIC);
+
+    if (settings.cent.user) {
+        subscribeLegacyChannels(CHANNELS_TYPE_PRIVATE);
+    }
+
+    sock.connect();
+
+    sock.onmessage = function (response) {
+        const json = JSON.parse(response.data);
+        $bus.$emit?.(`websocket.${json.type}`, json);
+    };
+}
+
+function buildNotificationCenterChannel({ clientName, channel, user, isPrivate }: {
+    clientName: string;
+    channel: string;
+    user: string;
+    isPrivate: boolean;
+}) {
+    const suffix = isPrivate ? `#${user}` : "";
+
+    return `ws:${clientName}:${channel}${suffix}`;
+}
+
+function subscribeNotificationCenterChannels(
+    client: Centrifuge,
+    channelsType: ChannelType,
+    settings: INotificationCenterSubscription,
+) {
+    const isPrivate = channelsType === CHANNELS_TYPE_PRIVATE;
+
+    CHANNELS_BY_TYPE[channelsType].forEach((channel) => {
+        const channelName = buildNotificationCenterChannel({
+            clientName: settings.client_name,
+            channel,
+            user: settings.user,
+            isPrivate,
+        });
+
+        const sub = client.newSubscription(channelName, {});
+
+        sub.on("publication", (ctx) => {
+            $bus.$emit?.(`websocket.${channel}`, ctx.data);
+        });
+
+        sub.subscribe();
+    });
+}
+
+async function startNotificationCenterFlow() {
+    try {
+        const settings = await loadNotificationCenterSubscriptionReq();
+
+        if (!settings?.url || !settings?.token || !settings?.client_name) {
+            return false;
+        }
+
+        const client = new Centrifuge(
+            `${settings.url}/connection/websocket`,
+            { token: settings.token },
+        );
+
+        subscribeNotificationCenterChannels(client, CHANNELS_TYPE_PUBLIC, settings);
+
+        if (settings.user) {
+            subscribeNotificationCenterChannels(client, CHANNELS_TYPE_PRIVATE, settings);
+        }
+
+        client.connect();
+        notificationCenterClient = client;
+
+        return true;
+    } catch (err) {
+        log.error("START_NOTIFICATION_CENTER_REALTIME_ERROR", err);
+        return false;
+    }
 }
 
 async function start() {
     if (typeof location === "undefined") {
         return;
     }
-    const { getSettings } = storeToRefs(useUserInfo());
 
-    const settings = getSettings.value;
     const { useMocker } = useEnvironments();
 
-
-    if (!settings || !settings.cent || useMocker) {
+    if (useMocker) {
         return;
     }
 
-    if (sock._clientID) {
+    disconnect();
+
+    const shouldUseNotificationCenter = false;// await shouldUseNotificationCenterFlow();
+
+    if (shouldUseNotificationCenter) {
+        const isStarted = await startNotificationCenterFlow();
+
+        if (isStarted) {
+            return;
+        }
+    }
+
+    startLegacyFlow();
+}
+
+async function shouldUseNotificationCenterFlow() {
+    const { infoProject } = storeToRefs(useCommon());
+
+    await loadProjectInfo();
+
+    return infoProject.value?.notification_center?.new_realtime_notifications_flow_enabled === true;
+}
+
+function disconnect() {
+    if (sock?._clientID) {
         sock.disconnect();
-        sock = {};
     }
 
-    sock = new Centrifuge({
-        ...settings.cent,
-        url: getCentrifugeUrl(settings.cent.url),
-    });
-    subscribe(CHANNELS_TYPE_PUBLIC);
-
-    if (settings.cent.user) {
-        subscribe(CHANNELS_TYPE_PRIVATE);
+    if (notificationCenterClient) {
+        notificationCenterClient.disconnect();
     }
 
-    sock.connect();
-
-    sock.onmessage = function(response) {
-        const json = JSON.parse(response.data);
-        $bus.$emit(`websocket.${ json.type }`, json);
-    };
+    sock = null;
+    notificationCenterClient = null;
 }
 
 export default {
     init,
     start,
-    subscribe,
 };
