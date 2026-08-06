2364b75cb410af6d57f2d185b3f143300236f5e9
Eduard Grabchak <edgrwork@gmail.com>
2026-08-06 10:01:25 +0300

    updated baseUrlHelper

--- src/helpers/BaseUrlHelper.ts
+++ src/helpers/BaseUrlHelper.ts
@@ -2,14 +2,12 @@ import { LOCALES } from "@theme/configs/constsLocales";
 
 export function getBaseUrl(url: string): string {
     const localeFromURL = String(url).split("/")[1];
-
-    return Object.values(LOCALES).includes(localeFromURL) && `/${ localeFromURL }` || "/";
+    return Object.values(LOCALES).includes(localeFromURL) && `/${ localeFromURL }/` || "/";
 }
 
 export function replaceBaseUrl(path: string): string {
     const localeFromURL = String(path).split("/")[1];
-    // eslint-disable-next-line no-useless-escape
-    const regexp = new RegExp("/" + localeFromURL + "(\/)?");
+    const regexp = new RegExp("/" + localeFromURL + "/?");
 
     return Object.values(LOCALES).includes(localeFromURL) ? path.replace(regexp, "/") : path;
 }
