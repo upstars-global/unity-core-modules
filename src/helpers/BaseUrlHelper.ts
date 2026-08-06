efb9193d2975731f71edd08fc3224e662f2fd975
m-ochkolyas-upstars <m.ochkolyas@upstars.com>
2026-08-06 11:30:52 +0300

    done

--- src/helpers/BaseUrlHelper.ts
+++ src/helpers/BaseUrlHelper.ts
@@ -7,7 +7,8 @@ export function getBaseUrl(url: string): string {
 
 export function replaceBaseUrl(path: string): string {
     const localeFromURL = String(path).split("/")[1];
-    const regexp = new RegExp("/" + localeFromURL + "/?");
+     
+    const regexp = new RegExp("/" + localeFromURL + "(/)?");
 
     return Object.values(LOCALES).includes(localeFromURL) ? path.replace(regexp, "/") : path;
 }
